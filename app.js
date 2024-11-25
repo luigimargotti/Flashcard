fa6b58b40b943323d4688a5aac7a49d69cb7b981
luigimargotti22 <141339755+luigimargotti@users.noreply.github.com>
2024-11-25 09:06:09 -0300

    .

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
     </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
